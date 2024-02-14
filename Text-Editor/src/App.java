9ad76989b2768eec278c6fb686af2be6c9451fcf
Joaquin Labarta <labartajoaquin@gmail.com>
2024-02-13 23:35:44 -0300

    TpFinal

--- Text-Editor/src/App.java
+++ Text-Editor/src/App.java
@@ -1,4 +1,10 @@
 import javax.swing.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -10,7 +16,6 @@ public class App {
 
 //----------------------------------------Constructores-----------------------------------------
 
-//Ventana
 class Marco extends JFrame{
     public Marco() {
         setBounds(300,300,300,300);
@@ -58,11 +63,16 @@ class Panel extends JPanel{
         creaItem("Light", "apariencia", "blanco");
         creaItem("Dark", "apariencia", "negro");
 
+
+        
         panelmenu.add(menu);
 
         //--------------------Ventana-------------------------------
         tPane = new JTabbedPane();
-        creaPanel();
+        listfile = new ArrayList<File>();
+        listAreaTexto = new ArrayList<JTextPane>();
+        listScroll = new ArrayList<JScrollPane>();
+        //-----------------------------------------------------------
         add(menu);
         add(tPane);
     }
@@ -72,6 +82,70 @@ class Panel extends JPanel{
         elementoItem = new JMenuItem(rotulo);
         if (menu.equals("archivo")) {
             archivo.add(elementoItem);
+            if (accion.equals("nuevo")) {
+                elementoItem.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e){
+                        creaPanel(); //crea area de texto
+                    }
+                });
+            }
+            else if(accion.equals("abrir")) {
+                elementoItem.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e){
+                        creaPanel();
+                        JFileChooser selectorArchivo = new JFileChooser();
+                        selectorArchivo.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                        int resultado = selectorArchivo.showOpenDialog(listAreaTexto.get(tPane.getSelectedIndex()));
+
+                        if (resultado == JFileChooser.APPROVE_OPTION) {
+                            //try {
+                                boolean existePath = false;
+
+                                for (int i=0;i<tPane.getTabCount(); i++) {
+                                    File f = selectorArchivo.getSelectedFile();
+                                    if (listfile.get(i).getPath().equals(f.getPath())) {
+                                        existePath = true;
+                                    }
+                                }
+
+                                if (!existePath) {
+                                    File archivo = selectorArchivo.getSelectedFile();
+                                    listfile.set(tPane.getSelectedIndex(), archivo);
+                                    try {
+                                        FileReader entrada = new FileReader(listfile.get(tPane.getSelectedIndex()).getPath());
+                                    } catch (IOException e1) {
+                                        // TODO Auto-generated catch block
+                                        e1.printStackTrace();
+                                    }
+                                }
+
+                            //} catch (IOException el) {
+                                // TODO: handle exception
+                            //}
+                        }
+                        
+                    }
+                });
+            }
+            else if(accion.equals("guardar")) {
+                elementoItem.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e){
+                        
+                    }
+                });
+            }
+            else if(accion.equals("guardarComo")) {
+                elementoItem.addActionListener(new ActionListener() {
+                    @Override 
+                    public void actionPerformed(ActionEvent e){
+                        
+                    }
+                });
+            }
+            
         }
         else if (menu.equals("editar")) {
             editar.add(elementoItem);
@@ -90,16 +164,30 @@ class Panel extends JPanel{
     //---------Metodo para crear la ventana de texto---------------
     public void creaPanel(){
         ventana = new JPanel(); //instancio la ventana
-        areaTexto = new JTextPane(); //instancio el area de texto
+        
+        listfile.add(new File(""));
+        listAreaTexto.add(new JTextPane());
+        listScroll.add(new JScrollPane(listAreaTexto.get(contadorPanel)));
 
-        ventana.add(areaTexto); //agrego el area de texto a la ventana
-
-        tPane.addTab("Page",ventana);
+        ventana.add(listScroll.get(contadorPanel));  
+    
+        tPane.addTab("Page " + (contadorPanel + 1),ventana);
+        tPane.setSelectedIndex(contadorPanel);
+        contadorPanel++;
+        existePanel = true;
     }
 
+    //---------Elementos funcionales--------
+    private int contadorPanel = 0;
+    private boolean existePanel = false;
+
+    //---------Elementos visuales-----------
     private JTabbedPane tPane;
     private JPanel ventana;
-    private JTextPane areaTexto;
+    //private JTextPane areaTexto;
+    private ArrayList<JTextPane> listAreaTexto;
+    private ArrayList<File> listfile;
+    private ArrayList<JScrollPane> listScroll;
     private JMenuBar menu;
     private JMenu archivo, editar, seleccion, ver, apariencia;
     private JMenuItem elementoItem;
---
+++ Text-Editor/src/fileReader.java
@@ -0,0 +1,8 @@
+
+public class fileReader {
+
+    public fileReader(String path) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
